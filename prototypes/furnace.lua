facada7e1af29c8029da628185b84432b5a34c28
Kumori <kumori@ryougi.com>
2016-04-10 16:01:11 -0400

    reverted furnace to hardcoded

--- prototypes/furnace.lua
+++ prototypes/furnace.lua
@@ -1,5 +1,4 @@
 
--- make all furnaces 3 inventory
-for i,v in ipairs(data.raw["furnace"]) do
-  v[i].result_inventory_size = 3;
-end
+data.raw["furnace"]["stone-furnace"].result_inventory_size = 3
+data.raw["furnace"]["steel-furnace"].result_inventory_size = 3
+data.raw["furnace"]["electric-furnace"].result_inventory_size = 3
