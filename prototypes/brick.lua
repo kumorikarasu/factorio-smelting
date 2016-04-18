af529198a83b77131d273f622ca889eab7bfd080
Kumori <kumori@ryougi.com>
2016-04-17 23:17:01 -0400

    added clay bricks smelting receipe

---
+++ prototypes/brick.lua
@@ -0,0 +1,13 @@
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "clay-brick",
+    category = "smelting",
+    energy_required = 3.5,
+    enabled = true,
+    ingredients = {{"clay", 4}},
+    result = "brick"
+  },
+}
+)
